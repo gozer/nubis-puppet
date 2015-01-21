7ddd863be2e5d4a016c5de8f321175badf63716d
Brian Hourigan <bhourigan@gmail.com>
2015-01-20 19:26:31 -0500

    Removing some cruft I forgot to trim when I initially copied the files

--- manifests/fluentd.pp
+++ manifests/fluentd.pp
@@ -1,23 +1,3 @@
-class m {
-
-}
-
-class { 'consul':
-  version => "0.4.1",
-
-  config_hash => {
-      'data_dir'   	=> '/var/lib/consul',
-      'log_level'  	=> 'INFO',
-      'ui_dir'      	=> '/var/lib/consul/ui',
-      'client_addr' 	=> '0.0.0.0',
-      'server'      	=> false,
-      'enable_syslog' 	=> true,
-  }
-}
-
-# Need auto-join helper too, here for now, but needs to be merged into
-# puppet somehow... Not sure it's worth merging upstream, however.
-
 include ::fluentd
 
 fluentd::source { 'syslog_main': 
